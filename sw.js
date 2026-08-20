d63ddbbe203ef150160d620440e600fc66881ba4
Koen <monique@joshharm.nl>
2026-08-20 16:17:43 +0200

    Calculatie: halve aantallen, mobiele indeling; notities: audio-upload en foutmeldingen

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ try {
   });
 } catch (e) { /* messaging niet beschikbaar */ }
 
-const CACHE_NAME = 'werkplaats-v17';
+const CACHE_NAME = 'werkplaats-v21';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
