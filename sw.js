284ba92782198134284cfb097bf4ffedd722f94d
Koen <monique@joshharm.nl>
2026-08-25 09:24:56 +0200

    Post-it verschijnt meteen na slepen naar het bord

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ try {
   });
 } catch (e) { /* messaging niet beschikbaar */ }
 
-const CACHE_NAME = 'werkplaats-v24';
+const CACHE_NAME = 'werkplaats-v25';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
