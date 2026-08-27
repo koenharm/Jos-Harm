eabde20ca73e8a23162efab2c801e8271daab4ff
Koen <monique@joshharm.nl>
2026-08-27 17:16:56 +0200

    Formulierenscherm opnieuw ingedeeld + iconen (v34)

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ try {
   });
 } catch (e) { /* messaging niet beschikbaar */ }
 
-const CACHE_NAME = 'werkplaats-v32';
+const CACHE_NAME = 'werkplaats-v34';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
